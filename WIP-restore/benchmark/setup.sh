84d20757e59143e9118e52db9b88b2546553b7eb
Carl Johnsen <carl-johannes@di.ku.dk>
2025-03-01 09:58:17 +0100

    Updated the git commit used in the restore benchmark

--- WIP-restore/benchmark/setup.sh
+++ WIP-restore/benchmark/setup.sh
@@ -2,7 +2,7 @@ mkdir data_repos
 cd data_repos
 git clone git@github.com:duplicati/duplicati.git duplicati
 cd duplicati
-git checkout 5aa06f3
+git checkout 758b3ed
 dotnet build -c Release
 cd Tools/TestDataGenerator
 dotnet build -c Release
