4ebdc4ab51fbab3802bea1244c916ea4086e07db
Carl Johnsen <carl-johannes@di.ku.dk>
2025-03-04 19:59:15 +0100

    Removed unused imports

--- WIP-restore/benchmark/runner/Program.cs
+++ WIP-restore/benchmark/runner/Program.cs
@@ -1,11 +1,7 @@
-using System;
 using System.CommandLine;
 using System.CommandLine.NamingConventionBinder;
 using System.Diagnostics;
-using System.Threading.Tasks;
-using Duplicati;
 using Duplicati.Library.Main;
-using Google.Type;
 
 namespace Runner
 {
