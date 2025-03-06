feeba2ec0cf3888e9223743cd5ce35fe13308b94
Carl Johnsen <carl-johannes@di.ku.dk>
2025-03-06 15:31:38 +0100

    Added the tuning benchmark

--- WIP-restore/benchmark/runner/Program.cs
+++ WIP-restore/benchmark/runner/Program.cs
@@ -15,6 +15,7 @@ namespace Runner
             Operation Operation,
             string Output,
             Size Size,
+            string Tuning,
             Legacy VersionToTest
         );
 
@@ -31,7 +32,8 @@ namespace Runner
             DatasetOnly,
             Filesizes,
             Regular,
-            Sparsity
+            Sparsity,
+            Tuning
         }
 
         private enum Size
@@ -132,10 +134,11 @@ namespace Runner
                 new Option<string>(aliases: ["--data-generator"], description: "Path to the data generator executable", getDefaultValue: () => "../data_repos/duplicati/Tools/TestDataGenerator/bin/Release/net8.0/TestDataGenerator") { Arity = ArgumentArity.ExactlyOne },
                 new Option<string>(aliases: ["--hostname"], description: "Hostname of the machine running the benchmark", getDefaultValue: () => System.Net.Dns.GetHostName()) { Arity = ArgumentArity.ExactlyOne },
                 new Option<int>(aliases: ["--iterations", "-i"], description: "Number of iterations", getDefaultValue: () => 1),
-                new Option<Operation>(aliases: ["--operation"], description: "Operation to perform. Should be one of: datasetonly, filesizes, regular, sparsity", getDefaultValue: () => Operation.Regular) { Arity = ArgumentArity.ExactlyOne },
+                new Option<Operation>(aliases: ["--operation"], description: "Operation to perform. Should be one of: datasetonly, filesizes, regular, sparsity, tuning", getDefaultValue: () => Operation.Regular) { Arity = ArgumentArity.ExactlyOne },
                 new Option<string>(aliases: ["--output", "-o"], description: "Output directory to hold the generated files and the results", getDefaultValue: () => "..") { Arity = ArgumentArity.ExactlyOne },
                 new Option<Size>(aliases: ["--size", "-s"], description: "Size of the test data. Should one of: all, small, medium, large", getDefaultValue: () => Size.Small) { Arity = ArgumentArity.ExactlyOne },
-                new Option<Legacy>(aliases: ["--version-to-test", "-vtt"], description: "Version of the restore flow to test. Should be one of: both, new, legacy, prenewbackend. Both runs legacy first, followed by new.", getDefaultValue: () => Legacy.Both) { Arity = ArgumentArity.ExactlyOne }
+                new Option<string>(aliases: ["--tuning"], description: "The concurrency parameters to test. Should be a comma separated string: <FileProcessors>,<VolumeDownloaders>,<VolumeDecryptors>,<VolumeDecompressors>", getDefaultValue: () => "1,1,1,1") { Arity = ArgumentArity.ExactlyOne },
+                new Option<Legacy>(aliases: ["--version-to-test", "-vtt"], description: "Version of the restore flow to test. Should be one of: both, new, legacy, prenewbackend. Both runs legacy first, followed by new.", getDefaultValue: () => Legacy.Both){ Arity = ArgumentArity.ExactlyOne }
             };
 
             root_cmd.Handler = CommandHandler.Create(Run);
@@ -266,6 +269,8 @@ namespace Runner
                     return await RunRegular(config);
                 case Operation.Sparsity:
                     return await RunSparsity(config);
+                case Operation.Tuning:
+                    return await RunTuning(config);
                 default:
                     throw new ArgumentException($"Invalid operation provided: {config.Operation}");
             }
@@ -562,6 +567,153 @@ namespace Runner
             }
 
             DeleteAll(data_dir);
+
+            return 0;
+        }
+
+        private static async Task<int> RunTuning(Config config)
+        {
+            var legacies = ParseLegaciesToRun(config.VersionToTest);
+            if (legacies.Length != 1)
+                throw new ArgumentException("Tuning only supports a single legacy version to test");
+            var legacy_str = legacies[0];
+            var sw = new Stopwatch();
+            var duplicati_options = DefaultOptions();
+            var (data_dir, times_dir) = DefaultDirs(config);
+            var datagen = GetDatagen(config);
+            var size_str = config.Size.ToString().ToLower();
+            if (config.Size == Size.All)
+                throw new ArgumentException("Tuning does not support running all sizes");
+            string[] tuning = config.Tuning.Split(',');
+            if (tuning.Length != 4)
+                throw new ArgumentException("Invalid tuning parameters provided. Should be a comma separated string: <FileProcessors>,<VolumeDownloaders>,<VolumeDecryptors>,<VolumeDecompressors>");
+            var log_file = Path.Combine(data_dir, $"profiling_{string.Join("_", tuning)}.log");
+
+            if (File.Exists(log_file))
+                File.Delete(log_file);
+
+            sw.Start();
+            var generated = await GenerateData(datagen, config.Size, data_dir);
+            sw.Stop();
+            using (var writer = new StreamWriter(Path.Combine(times_dir, $"{config.Hostname}_{size_str}_generate.csv"), true))
+                writer.WriteLine(sw.ElapsedMilliseconds);
+
+            string backup_dir = Path.Combine(data_dir, $"backup_{size_str}");
+            string restore_dir = Path.Combine(data_dir, $"restore_{size_str}");
+
+            if (!Directory.Exists(backup_dir))
+            {
+                sw.Restart();
+                BackupData(generated, backup_dir, duplicati_options);
+                sw.Stop();
+                using var writer = new StreamWriter(Path.Combine(times_dir, $"{config.Hostname}_{size_str}_backup.csv"), true);
+                writer.WriteLine(sw.ElapsedMilliseconds);
+            }
+
+            duplicati_options["restore-channel-buffer-size"] = "4096";
+            duplicati_options["internal-profiling"] = "true";
+            duplicati_options["log-file-log-filter"] = "+[.*InternalTimings.*]:+[.*RestoreNetwork.*]:-[.*]";
+            duplicati_options["log-file-log-level"] = "Profiling";
+            duplicati_options["log-file"] = log_file;
+            duplicati_options["restore-file-processors"] = tuning[0];
+            duplicati_options["restore-volume-downloaders"] = tuning[1];
+            duplicati_options["restore-volume-decryptors"] = tuning[2];
+            duplicati_options["restore-volume-decompressors"] = tuning[3];
+            DeleteAll(restore_dir);
+
+            Console.WriteLine($"Running restore with tuning parameters: {string.Join(", ", tuning)}");
+            RestoreData(backup_dir, restore_dir, duplicati_options, legacy_str);
+
+            using var reader = new StreamReader(log_file);
+            string? line;
+            Dictionary<string, (List<long>, List<long>)> timings = new()
+            {
+                ["FileProcessor"] = ([], []),
+                ["VolumeDownloader"] = ([], []),
+                ["VolumeDecryptor"] = ([], []),
+                ["VolumeDecompressor"] = ([], [])
+            };
+            List<long> wall_clock = [];
+            if (reader != null)
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains("took"))
+                    {
+                        // Parse the total ms from the last token which is in 0:00:00:00.000 format
+                        var time_str = line.Split(' ')[^1];
+                        var time = TimeSpan.Parse(time_str);
+                        wall_clock.Add((long)time.TotalMilliseconds);
+                        continue;
+                    }
+
+                    // Remove the first 78 characters, as they are the timestamp and common
+                    line = line[78..];
+                    int idx = line.IndexOf('-');
+                    var process_name = line[..idx];
+                    var tokens = line[idx..].Split(' ');
+                    var times = tokens
+                        .Where(x => x.Contains("ms") || x.Contains("ms,"))
+                        .Select(x => int.Parse(new string([.. x.Where(y => char.IsDigit(y))])))
+                        .ToArray();
+
+                    var (times_list, read_list) = timings.GetValueOrDefault(process_name, ([], []));
+                    var found = true;
+
+                    switch (process_name)
+                    {
+                        case "FileProcessor":
+                            var timesum = times[1..3].Concat(times[5..8]).Concat(times[9..]).Sum();
+                            times_list.Add(timesum);
+                            read_list.Add(times[4]);
+                            break;
+                        case "VolumeDownloader":
+                            times_list.Add(times[2]);
+                            read_list.Add(times[0]);
+                            break;
+                        case "VolumeDecryptor":
+                            times_list.Add(times[1]);
+                            read_list.Add(times[0]);
+                            break;
+                        case "VolumeDecompressor":
+                            times_list.Add(times[2..].Sum());
+                            read_list.Add(times[0]);
+                            break;
+                        default:
+                            found = false; // Ignore
+                            break;
+                    }
+
+                    if (found)
+                        timings[process_name] = (times_list, read_list);
+                }
+
+            foreach (string process in new string[] { "FileProcessor", "VolumeDownloader", "VolumeDecryptor", "VolumeDecompressor" })
+            {
+                var (times, reads) = timings[process];
+                var times_str = times.Average();//string.Join(";", times);
+                var reads_str = reads.Average();//string.Join(";", reads);
+                Console.WriteLine($"{process}: {reads_str} - {times_str}");
+            }
+
+            var total_work = timings.Select(x => x.Value.Item1.Average()).Sum();
+            var total_read = timings.Select(x => x.Value.Item2.Average()).Average();
+            Console.WriteLine($"Total work: {total_read} - {total_work}");
+
+            Console.WriteLine($"[Wall clock] - setup {wall_clock[0]}, restore {wall_clock[1]}, total {wall_clock.Sum()}");
+
+            Console.WriteLine($"Approximate hidden overhead: {wall_clock.Sum() - total_work}");
+
+            // Strategy is: if read is largest, bump up fileprocessors, otherwise, bump up the largest workload
+            if (total_read > total_work)
+            {
+                tuning[0] = (int.Parse(tuning[0]) * 2).ToString();
+            }
+            else
+            {
+                var max_idx = timings.Skip(1).Select((x, i) => (x.Value.Item1.Average(), i)).Max().i + 1;
+                tuning[max_idx] = (int.Parse(tuning[max_idx]) * 2).ToString();
+            }
+            Console.WriteLine($"New tuning parameters: {string.Join(", ", tuning)}");
 
             return 0;
         }
