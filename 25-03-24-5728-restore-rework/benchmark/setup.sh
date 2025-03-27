b8a8d9bb1585148a763574ee4bfbb76078ffb057
Carl Johnsen <carl-johannes@di.ku.dk>
2025-03-27 07:04:06 +0100

    Added new restore bugfix to the list, updated the canary version links, and updated commit hashes

--- 25-03-24-5728-restore-rework/benchmark/setup.sh
+++ 25-03-24-5728-restore-rework/benchmark/setup.sh
@@ -2,7 +2,7 @@ mkdir data_repos
 cd data_repos
 git clone git@github.com:duplicati/duplicati.git duplicati
 cd duplicati
-git checkout 354a804
+git checkout 9dc8c0d
 dotnet build -c Release
 cd Tools/TestDataGenerator
 dotnet build -c Release
