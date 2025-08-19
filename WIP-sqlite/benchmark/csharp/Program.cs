fd076a8b4e5aca40b3aac75b07be4fe7929d08c2
Carl Johnsen <carl-johannes@di.ku.dk>
2025-08-19 08:05:53 +0200

    Added new dotnet benchmark project

---
+++ WIP-sqlite/benchmark/csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using BenchmarkDotNet.Running;
+
+namespace sqlite_bench
+{
+
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+        }
+    }
+
+}
