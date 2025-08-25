c26911ac0a307d293471638a8d773921e103ce80
Carl Johnsen <carl-johannes@di.ku.dk>
2025-08-25 06:10:36 +0200

    Added windows run_all.bat

---
+++ WIP-sqlite/benchmark/run_all.bat
@@ -0,0 +1,34 @@
+@echo off
+
+set warmup=10000
+set repetitions=100000
+
+REM Define sizes array
+set sizes=10000 100000 1000000 10000000
+REM Define threads array
+set threads=1 2 4 8 16 32
+REM Define batches array
+set batches=0 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536
+
+for %%s in (%sizes%) do (
+    .\bin\schema1 --num-entries %%s --num-warmup %warmup% --num-repetitions %repetitions%
+    .\bin\schema4 --num-entries %%s --num-warmup %warmup% --num-repetitions %repetitions%
+    .\bin\schema7 --num-entries %%s --num-warmup %warmup% --num-repetitions %repetitions%
+    .\bin\schema10 --num-entries %%s --num-warmup %warmup% --num-repetitions %repetitions%
+    .\bin\pragmas --num-entries %%s --num-warmup %warmup% --num-repetitions %repetitions%
+    for %%t in (%threads%) do (
+        .\bin\parallel --num-entries %%s --num-warmup %warmup% --num-repetitions %repetitions% --num-threads %%t
+    )
+    for %%b in (%batches%) do (
+        .\bin\batching --num-entries %%s --num-warmup %warmup% --num-repetitions %repetitions% --num-batch %%b
+    )
+)
+
+REM Build C# project
+dotnet build -c Release csharp
+
+REM Run C# benchmark
+csharp\bin\Release\net9.0\sqlite_bench --buildTimeout 600
+
+REM Copy results
+copy BenchmarkDotNet.Artifacts\results\*.csv reports\
