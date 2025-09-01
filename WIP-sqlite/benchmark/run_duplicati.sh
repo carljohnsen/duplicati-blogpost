b8c4a6c011d9ece1464d85387f2c4f07c666ca69
Carl Johnsen <carl-johannes@di.ku.dk>
2025-09-01 15:48:37 +0200

    Added cache size to the previous duplicati version calls in run_duplicati

--- WIP-sqlite/benchmark/run_duplicati.sh
+++ WIP-sqlite/benchmark/run_duplicati.sh
@@ -15,11 +15,11 @@ fi
 dbpath=$(pwd)/benchmarking.sqlite
 rm -rf ../../data/restore ../../data/backup $dbpath* ~/.config/Duplicati/
 duplicati_cli=Executables/net8/Duplicati.CommandLine/bin/Release/net8.0/Duplicati.CommandLine
-$duplicati_cli backup ../../data/backup --passphrase=1234 --log-file=120-backup.log --log-file-log-level=Information --dbpath=$dbpath --dblock-size=1mb --blocksize=1kb ../../data/testdata
+$duplicati_cli backup ../../data/backup --passphrase=1234 --log-file=120-backup.log --log-file-log-level=Information --dbpath=$dbpath --dblock-size=1mb --blocksize=1kb ../../data/testdata --sqlite-page-cache=64mb
 rm $dbpath*
-$duplicati_cli repair ../../data/backup --passphrase=1234 --log-file=120-repair.log --log-file-log-level=Information
-$duplicati_cli restore ../../data/backup --passphrase=1234 --log-file=120-restore.log --log-file-log-level=Information --restore-path=../../data/restore --restore-channel-buffer-size=1024 --restore-file-processors=8
-$duplicati_cli delete ../../data/backup --passphrase=1234 --log-file=120-delete.log --log-file-log-level=Information --version=0 --allow-full-removal=true
+$duplicati_cli repair ../../data/backup --passphrase=1234 --log-file=120-repair.log --log-file-log-level=Information --sqlite-page-cache=64mb
+$duplicati_cli restore ../../data/backup --passphrase=1234 --log-file=120-restore.log --log-file-log-level=Information --restore-path=../../data/restore --restore-channel-buffer-size=1024 --restore-file-processors=8 --sqlite-page-cache=64mb
+$duplicati_cli delete ../../data/backup --passphrase=1234 --log-file=120-delete.log --log-file-log-level=Information --version=0 --allow-full-removal=true --sqlite-page-cache=64mb
 rm -rf ../../data/restore ../../data/backup $dbpath* ~/.config/Duplicati/
 
 git checkout v2.1.0.125_canary_2025-07-15
