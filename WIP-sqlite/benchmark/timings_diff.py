5b822c7b94dde6aa494cf8f2d196730c503c48a8
Carl Johnsen <carl-johannes@di.ku.dk>
2025-09-08 13:12:06 +0200

    Updated the timings script to use the new log summary files

--- WIP-sqlite/benchmark/timings_diff.py
+++ WIP-sqlite/benchmark/timings_diff.py
@@ -3,16 +3,28 @@ import os
 
 base_dir = 'lib/duplicati'
 operations = ['backup', 'repair', 'restore', 'delete']
+versions = ['120', '125']
+times = {ver: { op: { 'begin': -1, 'end': -1 } for op in operations} for ver in versions}
+
+time_format = None
+for ver in versions:
+    with open(f'{base_dir}/{ver}.log') as f:
+        for line in f:
+            toks = line.strip().lower().split(' ')
+            if toks[-2] != 'has' or (toks[-1] != 'started' and toks[-1] != 'completed'):
+                continue
+            op = toks[-3]
+            beginend = 'begin' if toks[-1] == 'started' else 'end'
+            print (line)
+            print (toks[1], op, beginend)
+            if time_format == None:
+                if ':' in toks[1]:
+                    time_format = '%H:%M:%S'
+                else:
+                    time_format = '%H.%M.%S'
+            times[ver][op][beginend] = datetime.datetime.strptime(toks[1], time_format)
+
 for op in operations:
-    times = []
-    for ver in ['120', '125']:
-        with open(f'{base_dir}/{ver}-{op}.log') as f:
-            begin, end = -1, -1
-            time_format = '%H.%M.%S' if os.name == 'nt' else '%H:%M:%S'
-            for line in f:
-                if line.strip().lower().endswith(f'the operation {op} has started'):
-                    begin = datetime.datetime.strptime(line.split(' ')[1], time_format)
-                elif line.strip().lower().endswith(f'the operation {op} has completed'):
-                    end = datetime.datetime.strptime(line.split(' ')[1], time_format)
-            times.append((end - begin).total_seconds())
-    print(f'{op} times: {times} seconds, diff: {times[1] - times[0]} seconds')
+    time_120 = (times['120'][op]['end'] - times['120'][op]['begin']).total_seconds()
+    time_125 = (times['125'][op]['end'] - times['125'][op]['begin']).total_seconds()
+    print(f'{op} times: [{time_120}, {time_125}] seconds, diff: {time_120 - time_125} seconds')
